c3c51d5c560ddcce99e55a1245d5c7206d08a6f8
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-07-23 00:46:08 +0100

    added basic style to main section

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,7 @@
+import "../components/main.css";
 const Main = () => {
   return (
-    <div>
+    <div className="article">
       <article>
         <h1>Let’s Go Outside </h1>
         <p>
@@ -8,12 +9,15 @@ const Main = () => {
           you’re looking for the thrill of a woodland glide on our Segway
           experience, testing your skill with archery and laser clay shooting or
           being amazed as you get hands-on and try birds of prey in a falconry
-          encounter! We’ve got lots of adventures and activities all designed
-          for small groups, families, schools, hen or stag parties or corporate
-          away days. Get outside this year and get some fresh air and adventure.
+          encounter!
         </p>
-        <button>READ MORE</button>
+        <p>
+          We’ve got lots of adventures and activities all designed for small
+          groups, families, schools, hen or stag parties or corporate away days.
+          Get outside this year and get some fresh air and adventure.
+        </p>
       </article>
+      <button>READ MORE</button>
     </div>
   );
 };
