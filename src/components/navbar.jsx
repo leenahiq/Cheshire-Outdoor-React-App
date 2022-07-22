cc8df38f2cad6f0a233db2972e6d1e553fe58305
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-07-22 23:49:27 +0100

    Added basic styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,18 @@
-import img1 from "../images/img1.jpg";
+import "../components/navbar.css";
+import img1 from "../images/img1.png";
 const Navbar = () => {
   return (
     <div>
       <div className="email">
         <p> events@cheshire-outdoors.info</p>
       </div>
-      <div className="nav">
-        <div>
-          <img src={img1} alt="logo" />
-        </div>
-        <div>
-          <a href="/">Home</a>
+      <div className="logo">
+        <img src={img1} alt="logo" />
+
+        <div className="nav">
+          <a href="/">
+            <b>Home</b>
+          </a>
           <a href="/">About</a>
           <a href="/">Bushcraft</a>
           <a href="/">Groups</a>
