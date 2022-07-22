83cbb4d755aa0e5bcd8598d3eb2b4f66878bafe0
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-07-23 00:10:56 +0100

    added basic style to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,9 @@
+import "../components/header.css";
 import img from "../images/img6.jpg";
+
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <img src={img} alt="img" />
     </div>
   );
