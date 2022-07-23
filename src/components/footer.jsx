12d394e78ece5847610611536f9eccc580483b37
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-07-23 13:46:24 +0100

    basic style to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,16 @@
+import "../components/footer.css";
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className="footer">
+      <div className="link">
         <a href="/">Term & Condition</a>
         <a href="/">Privacy Policy</a>
         <a href="/">Sitemap</a>
         <a href="/">Secure Payment</a>
       </div>
-      <div>
+      <div className="text">
         <p>Registered in England and Wales: No 08881823</p>
+
         <p>Copyright Cheshire Bushcraft ltd © 2020</p>
       </div>
     </div>
