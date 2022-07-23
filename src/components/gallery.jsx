51a28100ace7cfad0c87aae1168dcfe7863dda8a
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-07-23 13:15:35 +0100

    added basic style to gallery section

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,23 +1,22 @@
+import "../components/gallery.css";
 import img3 from "../images/img3.jpg";
 import img5 from "../images/img5.jpg";
 import img2 from "../images/img2.jpg";
 import img4 from "../images/img4.jpg";
+
 const Gallery = () => {
   return (
     <div>
-      <div>
-        <img src={img3} alt="img" />
-        <img src={img5} alt="img" />
+      <div className="img box1">
+        <img className="img3" src={img3} alt="img" />
+        <img className="img5" src={img5} alt="img" />
       </div>
-      <div>
-        <img src={img2} alt="img" />
-        <img src={img4} alt="img" />
+      <div className="img box2">
+        <img className="img2" src={img4} alt="img" />
+        <img className="img4" src={img2} alt="img" />
       </div>
     </div>
   );
 };
 
 export default Gallery;
-<div>
-  <div></div>
-</div>;
